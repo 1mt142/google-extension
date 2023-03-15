3ca029abbcbc651e6bed4302b370f4111ced7c13
Mohsin Khandoker <mohsin.khondoker@upaybd.com>
2023-03-15 16:14:55 +0600

    Fix Issues

--- clock/script.js
+++ clock/script.js
@@ -62,18 +62,32 @@ function addZero(i) {
 
 setInterval(digitalClock, 1000);
 
-// Get the button and the element by their ids
-const toggleButton = document.getElementById("toggleButton");
-const myDiv = document.getElementById("myDiv");
+// const toggleButton = document.getElementById("toggleButton");
+// const myDiv = document.getElementById("myDiv");
+// toggleButton.addEventListener("click", function () {
+//   if (myDiv.style.display === "none") {
+//     myDiv.style.display = "block";
+//     toggleButton.textContent = "Hide Calender";
+//   } else {
+//     myDiv.style.display = "none";
+//     toggleButton.textContent = "Show Calender";
+//   }
+// });
 
-// Add event listener to the button
-toggleButton.addEventListener("click", function () {
-  // Toggle the visibility of the element
-  if (myDiv.style.display === "none") {
-    myDiv.style.display = "block";
-    toggleButton.textContent = "Hide Calender";
-  } else {
-    myDiv.style.display = "none";
-    toggleButton.textContent = "Show Calender";
-  }
+const myDiv = document.getElementById("myDiv");
+const toggleButton = document.getElementById("toggleButton");
+
+toggleButton.addEventListener("click", () => {
+  // Get the current visibility state from localStorage (default to "visible")
+  const isVisible = localStorage.getItem("isVisible") !== "false";
+
+  // Toggle the visibility state and save it to localStorage
+  localStorage.setItem("isVisible", !isVisible);
+
+  // Set the visibility of the div based on the new state
+  myDiv.style.display = isVisible ? "none" : "block";
 });
+
+// On page load, set the initial visibility state of the div based on localStorage
+const isVisible = localStorage.getItem("isVisible") !== "false";
+myDiv.style.display = isVisible ? "block" : "none";
